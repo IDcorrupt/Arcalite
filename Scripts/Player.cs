74b4430c500a6b6c54f0811e36541f8217e4d8e2
IDcorrupt <katona.balint4@gmail.com>
2024-10-03 13:49:48 +0200

    movement ovehauled
    movement is now smooth
    crouch missing

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,55 +1,91 @@
 using Godot;
 using System;
+using System.Text;
 
 public partial class Player : CharacterBody2D
 {
-    private int _speed = 300;
-    private int jump_strength = 400;
+    private int max_speed = 500;
+    private int acceleration = 1600;
+    private int friction = 4000;
+    private int jump_strength = 610;
     private float GRAVITY = 2000f;
     
-    
-
-    public void GetInput()
+    public Vector2 inputExp()
     {
-        float direction = 0f;
-        if (Input.IsActionPressed("ui_right"))
-        {
-            direction = 1f;
-        }else if (Input.IsActionPressed("ui_left"))
-        {
-            direction = -1f;
-        }
-        if (Input.IsActionPressed("ui_select"))
+        Vector2 direction = new();
+
+        direction.X = Convert.ToInt32(Input.IsActionPressed("game_right")) - Convert.ToInt32(Input.IsActionPressed("game_left"));
+        if (Input.IsActionPressed("game_jump"))
         {
             if (IsOnFloor())
             {
-                Velocity = new Vector2 (Velocity.X, Velocity.Y + jump_strength);
+                direction.Y = -1;
             }
         }
-        Velocity = new Vector2(direction *_speed, Velocity.Y);
+        return direction;
+    }
+
+
+    public void playerMovement(double delta)
+    {
+        //get input
+        Vector2 input = inputExp();
+
+        if (input.X == 0)
+        {
+            //no input
+            if (Velocity.X > (friction * delta))
+            {
+                //add friction to slow player
+                //Velocity -= Velocity.Normalized() * (float)(friction*delta);
+                Velocity = new Vector2 (Velocity.X - (float)(friction * delta), Velocity.Y);
+            }
+            else if (Velocity.X < (friction * delta * -1))
+            {
+                Velocity = new Vector2(Velocity.X + (float)(friction * delta), Velocity.Y);
+            }
+            //stop so it doesn't start moving the other way
+            else Velocity = new Vector2(0, Velocity.Y);
+        }
+        else
+        {
+            //if input -> add corresponding velocity
+            Velocity = new Vector2(Velocity.X + input.X * (float)(acceleration * delta), Velocity.Y);
+
+        }
+        //limit max speed
+        if (Velocity.X > max_speed)
+        {
+            Velocity = new Vector2(max_speed, Velocity.Y);
+        }
+        else if (Velocity.X < -max_speed)
+        {
+
+            Velocity = new Vector2(-max_speed, Velocity.Y);
+        }
+        if (input.Y != 0)
+        {
+            Velocity = new Vector2 (Velocity.X,Velocity.Y -jump_strength);
+        }
+        //execute move
+        MoveAndSlide();
+
     }
 
     public void fall(double delta)
     {
-        //Velocity = new Vector2(Velocity.X, Velocity.Y + (float)(GRAVITY*delta));
-        Velocity = new Vector2 (Velocity.X, GRAVITY);
+        Velocity = new Vector2(Velocity.X, Velocity.Y + (float)(GRAVITY * delta));
     }
 
-
-
-
-
-    public bool getIsonFloor() { return IsOnFloor(); }
+    
     public override void _PhysicsProcess(double delta)
     {
-        //get inputs & set horizontal velocity
-        GetInput();
-        //set vertical velocity
+        //controls
         fall(delta);
-        
+        playerMovement(delta);
 
-        //move char
-        MoveAndSlide();
+        RichTextLabel text = GetNode<RichTextLabel>("./flordebug/isonfloorlabel");
+        text.Text = $"Velocity X: {Velocity.X} \n Velocity Y: {Velocity.Y}";
     }
 
 }
