2d04442ddad418c0ff9d25d1b323fb699d4661f6
KPM7 <>
2024-10-25 13:09:50 +0200

    register works :)

---
+++ Website/script/profile.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    $('#registerform').removeAttr('onsubmit').submit(function(event) {
+        event.preventDefault();
+        if ($('#password').val() != $('#passwordagain').val()) {
+            $('#pwdagain_error').show();
+        } else {
+            $('#pwdagain_error').hide();
+
+            let payload = {
+                username: $('#username').val(),
+                email: $('#email').val(),
+                password: $('#password').val()
+            }
+            
+            $.ajax({
+                type: "POST",
+                url: "api/register.php",
+                data: payload,
+                global: false,
+                success: (data) => {
+                    alert(data.message)
+                    $('#register_error').html("")
+                },
+                error: (data) => {
+                    console.log(data)
+                    $('#register_error').html(data.responseText)
+                }
+            });
+        }
+    });
+    
+});
