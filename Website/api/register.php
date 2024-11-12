a0e969fc0c07c5ab89cee458ac83b17b7e548085
KPM7 <>
2024-11-12 11:12:37 +0100

    Added login
    Added login and fixed up register.

--- Website/api/register.php
+++ Website/api/register.php
@@ -3,12 +3,12 @@
 require_once "config.php";
 
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
-    ReturnError(405, "HIBA! Hiba az API-hívásban.");
+    ReturnError(405, "Hiba az API-hívásban.");
 }
 
 if (!(count($_POST) == 3 && isset($_POST['username'],$_POST['email'],$_POST['password']))) {
     print_r($_POST);
-    ReturnError(400, "HIBA! Hiba az API-hívásban.");
+    ReturnError(400, "Hiba az API-hívásban.");
 }
 
 require_once "connection.php";
@@ -17,27 +17,30 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $email = $_POST['email'];
 
-$query = "SELECT COUNT(*) AS `db` FROM `profile` WHERE `username` = '$username'";
-$result = $db->query(query: $query);
-$num_of_users = intval($result->fetch_assoc()['db']);
-
-if ($num_of_users > 0) {
-    ReturnError(409, "HIBA! Már van fiók ilyen felhasználónévvel!");
-}
 
 $query = "SELECT COUNT(*) AS `db` FROM `profile` WHERE `email` = '$email'";
 $result = $db->query($query);
 $num_of_users = intval($result->fetch_assoc()['db']);
 
 if ($num_of_users > 0) {
-    ReturnError(409, "HIBA! Már van fiók regisztrálva ehhez az e-mail címhez!");
+    ReturnError(409, "Már van fiók regisztrálva ehhez az e-mail címhez!");
 }
+
+
+$query = "SELECT COUNT(*) AS `db` FROM `profile` WHERE `username` = '$username'";
+$result = $db->query(query: $query);
+$num_of_users = intval($result->fetch_assoc()['db']);
+
+if ($num_of_users > 0) {
+    ReturnError(409, "Már van fiók ilyen felhasználónévvel!");
+}
+
 
 $query = "INSERT INTO `profile` (`username`, `email`, `password`) VALUES ('$username','$email',PASSWORD('$password'))";
 $result = $db->query($query);
 
 if (!$result) {
-    ReturnError(500, "HIBA! Hiba az adatok feltöltése közben.");
+    ReturnError(500, "Hiba az adatok feltöltése közben.");
 }
 
 $response = array(
--- Website/script/profile.js
+++ Website/script/profile.js
@@ -4,38 +4,59 @@ $(document).ready(function() {
 
     $('#registerform').on('submit', function(event) {
         event.preventDefault();
-        if ($('#password').val() != $('#passwordagain').val()) {
+        
+        if ($('#reg_password').val() != $('#reg_passwordagain').val()) {
             $('#pwdagain_error').show();
-        } else {
-            $('#pwdagain_error').hide();
-
-            let payload = {
-                username: $('#username').val(),
-                email: $('#email').val(),
-                password: $('#password').val()
-            }
-            
-            $.ajax({
-                type: "POST",
-                url: "api/register.php",
-                data: payload,
-                global: false,
-                success: (data) => {
-                    $('#register_error').html("");
-                    alert(data.message);
-                    location.replace(location.href.split("?")[0]);
-                    location.reload();
-                },
-                error: (data) => {
-                    $('#register_error').html(data.responseText)
-                }
-            });
+            return;
         }
+
+        $('#pwdagain_error').hide();
+
+        let payload = {
+            username: $('#reg_username').val(),
+            email: $('#reg_email').val(),
+            password: $('#reg_password').val()
+        }
+        
+        $.ajax({
+            type: "POST",
+            url: "api/register.php",
+            data: payload,
+            global: false,
+            success: (data) => {
+                $('#register_error').html("");
+                alert(data.message);
+                location.replace(location.href.split("?")[0]);
+                location.reload();
+            },
+            error: (data) => {
+                $('#register_error').html(data.responseText)
+            }
+        });
+    
     });
 
     $('#loginform').on('submit', function(event) {
         event.preventDefault();
-        //weewoo
+
+        let payload = {
+            email: $('#login_email').val(),
+            password: $('#login_password').val()
+        }
+
+        $.ajax({
+            type: "GET",
+            url: "api/login.php",
+            data: payload,
+            global: false,
+            success: (data) => {
+                data = JSON.parse(data);
+                alert(data.id);
+            },
+            error: (data) => {
+                $('#login_error').html(data.responseText)
+            }
+        });
     });
     
 });
