3feae85468b7ebd0b45c66c4e354858ef5ba82ea
3111TAN-15 <3111TAN-15@tanuzem.boronkay.local>
2025-02-04 13:52:10 +0100

    Made SQL queries for fetching the ranking lists

--- Website/api/ranking.php
+++ Website/api/ranking.php
@@ -20,15 +20,36 @@ switch ($_GET["type"]) {
                     profile 
                     LEFT JOIN proach ON profile.id = proach.profileid
                     LEFT JOIN player ON profile.id = player.profileid
-                WHERE player.levelid = (SELECT MAX(id) FROM level)
                 GROUP BY profile.id;";
         break;
     case "game":
-        //ez nagyon hosszú lesz
-        $sql = "";
+        //[lang]-nál valahogy az oldal vagy az ember nyelvét meg kell adni :3
+        $sql = "SELECT
+                    player.name AS `Karakter`,
+                    profile.username AS `Profil`,
+                    avatardesc.name AS `Ąvatár`,
+                    player.playtime AS `Játékidő`,
+                    player.levelid AS `Elért szint`,
+                    COUNT(enemplay.enemyid) AS `Felfedezett ellenfelek`,
+                    COUNT(itemplay.itemid) AS `Felfedezett tárgyak`
+                FROM 
+                    player
+                    INNER JOIN profile ON player.profileid = profile.id
+                    INNER JOIN avatar ON player.avatarid = avatar.id
+                    INNER JOIN avatardesc ON avatardesc.avatarid = avatar.id
+                    INNER JOIN lang ON avatardesc.languageid = lang.id
+                    INNER JOIN enemplay ON enemplay.playerid = player.id
+                    INNER JOIN itemplay ON itemplay.playerid = player.id
+                WHERE lang = [lang];";
         break;
     default:
         break;
 }
 
 require_once "connection.php";
+
+$result = $db->query($sql);
+
+header("Content-Type: application/json");
+echo json_encode($result, JSON_PRETTY_PRINT);
+http_response_code(200);
