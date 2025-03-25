41e458a4079fe0f363a328254f0085e0da986d2d
Opi <bako.borka2005@tanulo.boronkay.hu>
2025-03-25 09:40:07 +0100

    haro

---
+++ Website/api/profile.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "config.php";
+
+checkValidity("GET", "userid", "langid");
+
+$userid = $_GET["userid"];
+$langid = $_GET["langid"];
+
+$sql = "SELECT player.id AS id, player.name AS name, player.hp AS hp, player.mp AS mp, player.levelid AS level, player.playtime AS playtime, avatar.splash AS image
+        FROM 
+            player 
+            INNER JOIN avatar ON player.avatarid = avatar.id
+            INNER JOIN avatardesc ON avatar.id = avatardesc.avatarid
+        WHERE 
+            avatardesc.languageid = $langid AND 
+            profileid = $userid";
+
+ReturnQuery($sql);
